2f46209ac66d6e29b6a86b1d8b13ad07c69eb3ac
treeaa <89380000+treeaa@users.noreply.github.com>
2024-02-22 14:36:21 +0800

    Delete examples/voiceUpdateAndPlayback directory

--- examples/voiceUpdateAndPlayback/VoiceBroadcast/Demo_key_mapping.h
+++
@@ -1,7 +0,0 @@
-#include ht68fv022.inc
-
-;****************************************
-;COMMAND TRIGGER DEFINE
-; HOW MANY COMMAND BE DEFINE
-;****************************************
-#define COMMANDABLE_NUM  0  ;;
